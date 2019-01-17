488ee52e95caa589c438db399619f289cd8a226a
C-D-Lewis <Bonsitm@gmail.com>
2019-01-17 17:23:10 +0000

    Update script, LICENSE, and package.json

--- index.js
+++ index.js
@@ -1,54 +1,94 @@
-const Mam = require('@iota/mam');
 const { asciiToTrytes } = require('@iota/converter');
 const hashJs = require('hash.js');
 const jsonSortify = require('json.sortify');
+const Mam = require('@iota/mam');
 
-let mamState = Mam.init('https://nodes.devnet.thetangle.org:443');
-mamState = Mam.changeMode(mamState, 'public');
+const NODE_ADDRESS = 'https://nodes.devnet.thetangle.org:443';
+const DEPTH = 3;
+const MWM = 9;
+const ACTION_TYPE_CONFIRMATION = '_sentToIOTA';
+
+/**
+ * Read a Thng by ID.
+ *
+ * @param {string} id - ID of the Thng to read.
+ * @returns {Promise} Promise that resolves the full Thng resource.
+ */
+const readThng = id => app.thng(id).read();
+
+/**
+ * Send the action's SHA 256 hash to IOTA.
+ *
+ * @param {object} action - The action to encode.
+ * @param {object} thng - The Thng the action is associated with.
+ * @returns {Promise} Promise that resolves the Thng, and the new MAM root.
+ */
+const sendToIOTA = (action, thng) => {
+  // Hash the action with SHA256
+  const sha256 = hashJs.sha256()
+    .update(jsonSortify(action))
+    .digest('hex');
+  logger.debug(`Action SHA256: ${sha256}`);
+
+  // Prepare the IOTA payload
+  const mamState = Mam.changeMode(Mam.init(NODE_ADDRESS), 'public');
+  const message = Mam.create(mamState, asciiToTrytes(sha256));
+
+  // Use pre-recorded MAM root, if any
+  if(thng.customFields && thng.customFields.iotaRoot) {
+    message.root = thng.customFields.iotaRoot;
+  }
+
+  return Mam.attach(message.payload, message.address, DEPTH, MWM)
+    .then(() => {
+      logger.debug(`MAM root: ${message.root}`);
+      return { thng, iotaRoot: message.root };
+    });
+};
+
+/**
+ * Update the Thng with the IOTA MAM root, if it doesn't already have one.
+ *
+ * @param {object} thng - The Thng to update.
+ * @param {string} iotaRoot - Root address for the MAM.
+ * @returns {Promise} Promise that resolves once the Thng is updated.
+ */
+const updateThngRoot = (thng, iotaRoot) => {
+  const customFields = Object.assign(thng.customFields || {}, { iotaRoot });
+  return (thng.customFields && thng.customFields.iotaRoot)
+    ? Promise.resolve(thng)
+    : app.thng(thng.id)
+        .update({ customFields });
+};
+
+/**
+ * Create a confirmation action containing the original action and the IOTA root
+ *
+ * @param {object} thng - The Thng to create an action on.
+ * @param {object} originalAction - The original action to include in the confirmation.
+ * @returns {Promise} Promise that resolves to the confirmation action.
+ */
+const createConfirmation = (thng, originalAction) => {
+  const payload = {
+    thng: thng.id,
+    type: ACTION_TYPE_CONFIRMATION,
+    customFields: { originalAction, iotaRoot: thng.customFields.iotaRoot },
+  };
+  return app.thng(thng.id)
+    .action(ACTION_TYPE_CONFIRMATION)
+    .create(payload)
+    .then(newAction => logger.info(`Confirmation action: ${newAction.id}`));
+};
 
 // @filter(onActionCreated) action.customFields.sendToIOTA=true
-function onActionCreated(event) {
-    sendToIOTA(event)
+const onActionCreated = (event) => {
+  const { action } = event;
+  readThng(action.thng)
+    .then(thng => sendToIOTA(action, thng))
+    .then(res => updateThngRoot(res.thng, res.iotaRoot))
+    .then(thng => createConfirmation(thng, action))
     .catch(err => logger.error(err.message || err.errors[0]))
     .then(done);
-}
-
-function sendToIOTA(event) {
-    // Convert event to trytes
-    const sha256 = hash(event.action);
-    logger.info('SHA of Action -- ' + sha256)
-    const trytes = asciiToTrytes(sha256);
-    const message = Mam.create(mamState, trytes);
-    const action = event.action;
-
-    // Fetch the Thng to see if we have a MAM root
-    return app.thng(action.thng).read()
-    .then(function (thng) {
-      if(thng.customFields.iotaRoot) {
-        const root = thng.customFields.iotaRoot;
-        logger.info('Found IOTA MAM root -- ' + root);
-        message.root = root;
-      } else {
-        // We did not have a MAM root because this is the first event in the MAM channel
-        logger.info('No IOTA MAM root');
-      }
-      return thng;
-    }).then(function(thng) {
-      Mam.attach(message.payload, message.address, 3, 9).then(function (){
-        if(!thng.customFields.iotaRoot) {
-          // Write MAM root to Thng
-          return app.thng(action.thng).update({customFields : {'iotaRoot' : message.root}});
-        }
-      }).catch(error => logger.error(error));
-    })
-  };
-
-function hash(data) {
-  return hashJs.sha256()
-    .update(jsonSortify(data))
-    .digest('hex');
-}
-
-module.exports = {
-  onActionCreated
 };
+
+module.exports = { onActionCreated };
