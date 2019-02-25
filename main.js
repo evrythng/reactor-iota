5aeb3e2dfa35ea0e17ac8f3e8670547b8d289569
Chris Lewis <Bonsitm@gmail.com>
2019-02-25 10:27:43 +0000

    Support Thng, product, or collection as the action target

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const DEPTH = 3;
 const MWM = 9;
 const CONFIRMATION_ACTION_TYPE = '_sentToIOTA';
 
-let action, thng;
+let action, target;
 
 /**
  * Send the action's SHA 256 hash to IOTA.
@@ -24,31 +24,47 @@ const sendToIOTA = () => {
 
   // Use pre-recorded MAM state, else use a new one
   let mamState = Mam.changeMode(Mam.init(NODE_ADDRESS), 'public');
-  if(thng.customFields && thng.customFields.iotaMamState) {
-    mamState = thng.customFields.iotaMamState;
+  if(target.customFields && target.customFields.iotaMamState) {
+    mamState = target.customFields.iotaMamState;
   }
-  
+
   // Encode the payload
-  const { root, payload, address } = Mam.create(mamState, asciiToTrytes(sha256));
-  return Mam.attach(payload, address, DEPTH, MWM).then(() => {
-    logger.debug(`root: ${root}`);
-    return { root, mamState };
-  });
+  try {
+    const { root, payload, address } = Mam.create(mamState, asciiToTrytes(sha256));
+    return Mam.attach(payload, address, DEPTH, MWM).then(() => {
+      logger.debug(`root: ${root}`);
+      return { root, mamState };
+    });
+  } catch (e) {
+    return Promise.reject('Failed to create and/or attach: ' + e.message + ' - ' + e.stack);
+  }
 };
 
 /**
- * Update the Thng with the IOTA MAM root, if it does not exist, and always update the MAM state.
+ * Update the target with the IOTA MAM root, if it does not exist, and always update the MAM state.
  *
  * @param {object} data - root and mamState from sendToIOTA().
- * @returns {Promise} Promise that resolves once the Thng is updated.
+ * @returns {Promise} Promise that resolves once the target is updated.
  */
-const updateThng = (data) => {
-  const customFields = Object.assign(thng.customFields || {}, { iotaMamState: data.mamState });
-  if (!thng.customFields.iotaRoot) {
+const updateTarget = (data) => {
+  if (!target.customFields) {
+    target.customFields = {};
+  }
+
+  const customFields = Object.assign(target.customFields, { iotaMamState: data.mamState });
+  if (!target.customFields.iotaRoot) {
     customFields.iotaRoot = data.root;
   }
 
-  return app.thng(thng.id).update({ customFields });
+  if (action.thng) {
+    return app.thng(target.id).update({ customFields });
+  }
+
+  if (action.collection) {
+    return app.collection(target.id).update({ customFields });
+  }
+
+  return app.product(target.id).update({ customFields });
 };
 
 /**
@@ -58,12 +74,19 @@ const updateThng = (data) => {
  */
 const createConfirmation = () => {
   const payload = {
-    thng: thng.id,
     type: CONFIRMATION_ACTION_TYPE,
-    customFields: { originalAction: action, iotaRoot: thng.customFields.iotaRoot },
+    customFields: { originalAction: action, iotaRoot: target.customFields.iotaRoot },
   };
-  return app.thng(thng.id)
-    .action(CONFIRMATION_ACTION_TYPE)
+
+  if (action.thng) {
+    payload.thng = action.thng;
+  } else if (action.product) {
+    payload.product = action.product;
+  } else if (action.collection) {
+    payload.collection = action.collection;
+  }
+
+  return app.action(CONFIRMATION_ACTION_TYPE)
     .create(payload)
     .then(newAction => logger.info(`Confirmation action: ${newAction.id}`));
 };
@@ -72,15 +95,29 @@ const createConfirmation = () => {
 const onActionCreated = (event) => {
   logger.info(`Sending action ${event.action.id} to IOTA`);
 
-  Promise.all([
-    app.action('all', event.action.id).read(),
-    app.thng(event.action.thng).read(),
-  ]).then((results) => {
-      action = results[0];
-      thng = results[1];
+  app.action('all', event.action.id).read()
+    .then((res) => {
+      action = res;
+
+      if (action.thng) {
+        return app.thng(action.thng).read();
+      }
+
+      if (action.product) {
+        return app.product(action.product).read();
+      }
+
+      if (action.collection) {
+        return app.collection(action.collection).read();
+      }
+
+      throw new Error('No target specified!');
+    })
+    .then((res) => {
+      target = res;
     })
     .then(sendToIOTA)
-    .then(updateThng)
+    .then(updateTarget)
     .then(createConfirmation)
     .catch(err => logger.error(err.message || err.errors[0]))
     .then(done);
