6f23055e5571fbefc2c1de96b15aa942b18aabc5
C-D-Lewis <Bonsitm@gmail.com>
2019-02-26 10:56:58 +0000

    Smarter way to handle multi target types

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const DEPTH = 3;
 const MWM = 9;
 const CONFIRMATION_ACTION_TYPE = '_sentToIOTA';
 
-let action, target;
+let action, target, targetType;
 
 /**
  * Send the action's SHA 256 hash to IOTA.
@@ -56,15 +56,7 @@ const updateTarget = (data) => {
     customFields.iotaRoot = data.root;
   }
 
-  if (action.thng) {
-    return app.thng(target.id).update({ customFields });
-  }
-
-  if (action.collection) {
-    return app.collection(target.id).update({ customFields });
-  }
-
-  return app.product(target.id).update({ customFields });
+  return app[targetType](target.id).update({ customFields });
 };
 
 /**
@@ -75,20 +67,38 @@ const updateTarget = (data) => {
 const createConfirmation = () => {
   const payload = {
     type: CONFIRMATION_ACTION_TYPE,
-    customFields: { originalAction: action, iotaRoot: target.customFields.iotaRoot },
+    [targetType]: action[targetType],
+    customFields: {
+      originalAction: action,
+      iotaRoot: target.customFields.iotaRoot,
+    },
   };
-
-  if (action.thng) {
-    payload.thng = action.thng;
-  } else if (action.product) {
-    payload.product = action.product;
-  } else if (action.collection) {
-    payload.collection = action.collection;
-  }
 
   return app.action(CONFIRMATION_ACTION_TYPE)
     .create(payload)
     .then(newAction => logger.info(`Confirmation action: ${newAction.id}`));
+};
+
+/**
+ * Read the complete target object from the action.
+ * This is either a Thng, product, or collection.
+ *
+ * @returns {Promise} A Promise that resolves once the process is complete.
+ */
+const readTarget = () => {
+  if (action.thng) {
+    targetType = 'thng';
+  } else if (action.product) {
+    targetType = 'product';
+  } else if (action.collection) {
+    targetType = 'collection';
+  } else {
+    return Promise.reject('No target was specified!');
+  }
+
+  return app[targetType](action[targetType]).read().then((res) => {
+    target = res;
+  });
 };
 
 // @filter(onActionCreated) action.customFields.sendToIOTA=true
@@ -98,24 +108,8 @@ const onActionCreated = (event) => {
   app.action('all', event.action.id).read()
     .then((res) => {
       action = res;
-
-      if (action.thng) {
-        return app.thng(action.thng).read();
-      }
-
-      if (action.product) {
-        return app.product(action.product).read();
-      }
-
-      if (action.collection) {
-        return app.collection(action.collection).read();
-      }
-
-      throw new Error('No target specified!');
     })
-    .then((res) => {
-      target = res;
-    })
+    .then(readTarget)
     .then(sendToIOTA)
     .then(updateTarget)
     .then(createConfirmation)
